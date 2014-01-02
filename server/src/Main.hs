f2f534dbd051f28bb2da045f4d69b8523c52bc5f
Nils Schweinsberg <mail@nils.cc>
2014-01-02 18:41:40 +0100

    Example updated

--- server/src/Main.hs
+++ server/src/Main.hs
@@ -7,11 +7,13 @@ import Control.Monad
 import qualified Data.ByteString.Char8 as B8
 
 import Network
+import Network.IRC.ByteString.Parser
 
 --import System.IO
 
 -- our imports:
 import IRC
+import IRC.Codes
 import Types
 
 user :: User
@@ -48,11 +50,67 @@ main = do
         mmsg <- getIncomingMessage con
         case mmsg of
           Nothing -> return ()
-          Just (_time, PrivMsg from to msg) -> do
+
+          Just (_time, MOTDMsg motd) -> do
+            putStrLn $ "MOTD: " ++ B8.unpack motd
+            loop
+
+          Just (_time, PrivMsg (Left from) to msg) -> do
             putStrLn $ B8.unpack to ++ ": "
-                       ++ "<" ++ show from ++ "> "
+                       ++ "<" ++ B8.unpack (userNick from) ++ "> "
                        ++ B8.unpack msg
             loop
+          Just (_time, PrivMsg (Right srvname) to msg) -> do
+            putStrLn $ B8.unpack to ++ ": "
+                       ++ "[" ++ B8.unpack srvname ++ "] "
+                       ++ B8.unpack msg
+            loop
+
+          Just (_time, NoticeMsg (Left from) to msg) -> do
+            putStrLn $ "NOTICE (" ++ B8.unpack to ++ "): "
+                       ++ "<" ++ B8.unpack (userNick from) ++ "> "
+                       ++ B8.unpack msg
+            loop
+          Just (_time, NoticeMsg (Right srvname) to msg) -> do
+            putStrLn $ "NOTICE (" ++ B8.unpack to ++ "): "
+                       ++ "[" ++ B8.unpack srvname ++ "] "
+                       ++ B8.unpack msg
+            loop
+
+          Just (_time, JoinMsg chan Nothing) -> do
+            putStrLn $ "> Joined " ++ show chan
+            loop
+          Just (_time, JoinMsg chan (Just who)) -> do
+            putStrLn $ "> " ++ B8.unpack (userNick who)
+                       ++ " joined " ++ show chan
+            loop
+
+          Just (_time, PartMsg chan who) -> do
+            putStrLn $ show who ++ " left " ++ show chan ++ "."
+            loop
+
+          Just (_time, KickMsg chan who comment) -> do
+            putStrLn $ "Kick! " ++ show who ++ " from " ++ show chan ++ "."
+                       ++ " Reason: " ++ show comment
+            loop
+
+          Just (_time, NickMsg (Just old) new) -> do
+            putStrLn $ "> " ++ B8.unpack (userNick old) ++ " is now called "
+                       ++ B8.unpack new
+            loop
+          Just (_time, NickMsg Nothing new) -> do
+            putStrLn $ "> You are now called " ++ B8.unpack new
+            loop
+
+          Just (_time, ErrorMsg code)
+            | code == err_NICKNAMEINUSE ||
+              code == err_NICKCOLLISION -> do
+              putStrLn $ "Nickname already taken."
+              loop
+            | otherwise -> do
+              putStrLn $ "EROR: " ++ show code
+              loop
+
    in void $ forkIO loop
 
   -- loop over incoming messages
@@ -60,6 +118,12 @@ main = do
         open <- isOpenConnection con'
         when open $ do
           con'' <- handleIncoming con'
-
           loop con''
-   in loop con
+   in void $ forkIO $ loop con
+
+  void getLine
+  sendNick con "irc2mobmob"
+  void getLine
+  sendNick con "McManiaC"
+  void getLine
+  closeConnection con (Just "Bye!")
