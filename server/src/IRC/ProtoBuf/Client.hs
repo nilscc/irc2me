c89df5ef5afad2a55c966f6f2eaaed39e4a1389e
Nils Schweinsberg <mail@nils.cc>
2014-01-04 00:59:55 +0100

    First client-request protobuf implementation

--- server/src/IRC/ProtoBuf/Client.hs
+++ server/src/IRC/ProtoBuf/Client.hs
@@ -1,2 +1,41 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveFoldable #-}
+{-# LANGUAGE StandaloneDeriving #-}
+
+{-# OPTIONS -fno-warn-orphans #-} -- FIXME
+
 -- | Module for client to server messages
 module IRC.ProtoBuf.Client where
+
+import Data.ProtocolBuffers
+import Data.TypeLevel.Num
+import Data.Word
+import Data.Foldable
+import Data.Monoid
+
+import GHC.Generics (Generic)
+
+-- FIXME: https://github.com/alphaHeavy/protobuf/issues/3
+deriving instance Foldable Last
+
+data Request
+  = SetOpMode
+  | GetBacklog
+  deriving (Eq, Enum, Show)
+
+data OpMode
+  = OpModeStandard
+  | OpModeBackground
+  deriving (Eq, Enum, Show)
+
+data PB_Request = PB_Request
+  { rq_request          :: Required D1  (Enumeration Request)
+    -- mode changes
+  , rq_opmode           :: Optional D10 (Enumeration OpMode)
+    -- message transfer
+  , rq_msg_max_backlog  :: Optional D20 (Value Word32)
+  }
+  deriving (Generic, Show)
+
+instance Encode PB_Request
+instance Decode PB_Request
