19e1b121564f6749915962a6d9618b034ab06fc0
Nils Schweinsberg <mail@nils.cc>
2014-05-30 12:47:50 +0000

    Update for networks table

--- server/src/Database/Tables/Networks.hs
+++ server/src/Database/Tables/Networks.hs
@@ -14,19 +14,18 @@ import IRC.Types
 
 data Network = Network
   { networkId     :: Integer
-  , networkName   :: String
   }
   deriving (Eq, Show, Ord)
 
 -- converters
 
 networkSELECT :: String
-networkSELECT = "SELECT id, name FROM networks"
+networkSELECT = "SELECT id FROM networks"
 
 toNetwork :: Converter Network
 toNetwork s = case s of
-  [SqlInteger i, SqlString n] -> Just $ Network i n
-  _                           -> Nothing
+  [SqlInteger i] -> Just $ Network i
+  _              -> Nothing
 
 -- queries
 
@@ -36,6 +35,14 @@ selectNetworks (Account a) = Query
   [toSql a]
   (convertList toNetwork)
 
+-- updates
+
+addNetwork :: Account -> String -> Update (Maybe Network)
+addNetwork (Account a) name = UpdateReturning
+  "INSERT INTO networks (account, name) VALUES (?, ?) \
+  \  RETURNING id"
+  [toSql a, toSql name]
+  (convertOne toNetwork)
 
 --------------------------------------------------------------------------------
 -- "network_servers" table
@@ -58,10 +65,26 @@ toServer s = case s of
 -- queries
 
 selectServers :: Network -> Query [Server]
-selectServers (Network i _) = Query
+selectServers (Network i) = Query
   (serverSELECT ++ " WHERE network = ?")
   [toSql i]
   (convertList toServer)
+
+-- updates
+
+addServer :: Network -> Server -> Update Bool
+addServer (Network n) server = Update
+  "INSERT INTO network_servers (network, address, port, use_ssl) \
+   \    VALUES (?, ?, ?, ?)"
+  [ toSql n
+  , toSql $ srv_host server
+  , toSql $ fromPortID $ srv_port server
+  , toSql $ srv_tls server == TLS
+  ]
+  (== 1)
+ where
+  fromPortID (PortNumber pn) = fromIntegral pn :: Integer
+  fromPortID _               = 6667
 
 --------------------------------------------------------------------------------
 -- "network_channels" table
@@ -79,13 +102,15 @@ toChannel s = case s of
 -- queries
 
 selectChannels :: Network -> Query [Channel]
-selectChannels (Network i _) = Query
+selectChannels (Network i) = Query
   (channelSELECT ++ " WHERE network = ?")
   [toSql i]
   (convertList toChannel)
 
+-- updates
+
 addChannel :: Network -> Channel -> Update Bool
-addChannel (Network i _) chan = Update
+addChannel (Network i) chan = Update
   "INSERT INTO network_channels (network, name) VALUES (?, ?)"
   [toSql i, toSql chan]
   (== 1)
