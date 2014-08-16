99833dc8f80018a8e89cd23cbb67f92fce70f659
Nils Schweinsberg <mail@nils.cc>
2014-08-16 21:45:57 +0200

    Clean up

--- desktop/form_main.cpp
+++ desktop/form_main.cpp
@@ -27,54 +27,12 @@ FormMain::FormMain(Irc2me &irc2me, QMainWindow &form_connect, QWidget *parent) :
             this, SLOT(quit()));
 
     ui->treeWidget_networklist->connectTo(irc2me);
-
-    // load view
-//    loadDefaultView();
 }
 
 FormMain::~FormMain()
 {
     delete ui;
 }
-
-/*
- * Views & layouts
- *
- */
-
-//void FormMain::loadDefaultView()
-//{
-////    QWidget *view = new QWidget();
-////    QBoxLayout *layout = new QBoxLayout(QBoxLayout::Direction::LeftToRight);
-
-////    // add network list
-////    NetworkList *netlis = new NetworkList();
-////    // connect to IRC
-////    netlis->connectTo(irc2me);
-////    layout->addWidget(netlis);
-
-////    // create and add channel input widget
-////    QWidget *chanInput = newChannelInputWidget();
-////    layout->addWidget(chanInput);
-
-////    // set central widget with new layout
-////    view->setLayout(layout);
-////    setCentralWidget(view);
-//}
-
-//QWidget* FormMain::newChannelInputWidget()
-//{
-////    QWidget *widget = new QWidget();
-////    QBoxLayout *topToBot = new QBoxLayout(QBoxLayout::Direction::TopToBottom);
-////    topToBot->setContentsMargins(0,0,0,0);
-
-////    // load channel view
-////    ChannelView *chan = new ChannelView();
-////    topToBot->addWidget(chan, 1);
-
-////    widget->setLayout(topToBot);
-////    return widget;
-//}
 
 /*
  * Slots
