a902c4482a7e4678b318ef0ff8ef4b57aefb1eda
Nils Schweinsberg <mail@nils.cc>
2014-09-02 00:51:34 +0200

    add prisms for SystemMsg

--- server/src/Irc2me/ProtoBuf/Messages/SystemMsg.hs
+++ server/src/Irc2me/ProtoBuf/Messages/SystemMsg.hs
@@ -1,7 +1,13 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module Irc2me.ProtoBuf.Messages.SystemMsg where
+
+import Control.Lens
 
 data SystemMsg
   = SystemMsg_Disconnect
   | SystemMsg_PING
   | SystemMsg_PONG
   deriving (Eq, Enum, Show)
+
+makePrisms ''SystemMsg
