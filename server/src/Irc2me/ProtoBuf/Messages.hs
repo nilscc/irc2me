ef74316a188aad61cbadfacce27f068ae4dd7c34
Nils Schweinsberg <mail@nils.cc>
2014-09-02 16:21:01 +0200

    missing IrcMessage in Messages

--- server/src/Irc2me/ProtoBuf/Messages.hs
+++ server/src/Irc2me/ProtoBuf/Messages.hs
@@ -2,6 +2,7 @@ module Irc2me.ProtoBuf.Messages
   ( module Irc2me.ProtoBuf.Messages.Client
   , module Irc2me.ProtoBuf.Messages.IrcIdentity
   , module Irc2me.ProtoBuf.Messages.IrcNetwork
+  , module Irc2me.ProtoBuf.Messages.IrcMessage
   , module Irc2me.ProtoBuf.Messages.Server
   , module Irc2me.ProtoBuf.Messages.System
   ) where
@@ -9,6 +10,7 @@ module Irc2me.ProtoBuf.Messages
 import Irc2me.ProtoBuf.Messages.Client
 import Irc2me.ProtoBuf.Messages.IrcIdentity
 import Irc2me.ProtoBuf.Messages.IrcNetwork
+import Irc2me.ProtoBuf.Messages.IrcMessage
 import Irc2me.ProtoBuf.Messages.Server
 import Irc2me.ProtoBuf.Messages.System
 
