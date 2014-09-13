5ae5196af32cb7ba89f078b1a5d6e1e8f175060e
Nils Schweinsberg <mail@nils.cc>
2014-09-14 01:34:52 +0200

    Basic account events

--- server/src/Irc2me/Events.hs
+++ server/src/Irc2me/Events.hs
@@ -1,3 +1,21 @@
 {-# LANGUAGE DataKinds #-}
 
 module Irc2me.Events where
+
+import Control.Concurrent.Event
+
+import Irc2me.Database.Tables.Accounts
+
+data AccountEvent = AccountEvent { _eventAccountId :: AccountID, _event :: Event }
+  deriving (Eq, Show)
+
+data Event
+  = ClientConnected
+  deriving (Eq, Show)
+
+type EventRW m a = EventT RW AccountEvent m a
+type EventWO m a = EventT WO AccountEvent m a
+
+--------------------------------------------------------------------------------
+-- IRC events
+
