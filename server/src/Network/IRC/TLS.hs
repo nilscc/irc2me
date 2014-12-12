43b7a22e21919ddd0e67706915ff20fbacbad44a
Nils Schweinsberg <mail@nils.cc>
2014-12-12 03:38:27 +0100

    Init TLS client params with global system cert store

--- server/src/Network/IRC/TLS.hs
+++ server/src/Network/IRC/TLS.hs
@@ -2,35 +2,17 @@
 
 module Network.IRC.TLS where
 
-import Data.List
 import Data.Default.Class
-import Data.X509
-import Data.X509.CertificateStore
 import Data.X509.Validation
+import System.X509
 
 import Network.TLS
 import Network.TLS.Extra
 
-certificationErrorsToIgnore :: [FailedReason]
-certificationErrorsToIgnore =
-  [ UnknownCA
-  , NameMismatch "irc.freenode.org"
-  ]
-
-type CertValidationFunction =
-     CertificateStore
-  -> ValidationCache
-  -> ServiceID
-  -> CertificateChain
-  -> IO [FailedReason]
-
-validateButIgnore :: CertValidationFunction
-validateButIgnore c v s ch = do
-  err <- validateDefault c v s ch
-  return $ err \\ certificationErrorsToIgnore
-
-clientParams :: HostName -> ClientParams
-clientParams hn = (defaultParamsClient hn "irc2me-server")
-  { clientSupported = def { supportedCiphers = ciphersuite_strong }
-  , clientHooks     = def { onServerCertificate = validateButIgnore }
-  }
+initClientParams :: HostName -> IO ClientParams
+initClientParams hn = do
+  cstore <- getSystemCertificateStore
+  return (defaultParamsClient hn "irc2me-server")
+    { clientSupported = def { supportedCiphers = ciphersuite_strong }
+    , clientShared    = def { sharedCAStore = cstore }
+    }
