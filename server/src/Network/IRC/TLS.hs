e90251278c60d338d95178b05d1e6785cace65dd
Nils Schweinsberg <mail@nils.cc>
2014-12-21 23:13:17 +0100

    Ignore UnknownCA errors

--- server/src/Network/IRC/TLS.hs
+++ server/src/Network/IRC/TLS.hs
@@ -2,12 +2,35 @@
 
 module Network.IRC.TLS where
 
+import Data.List
 import Data.Default.Class
+
+-- tls & related
+import Data.X509
+import Data.X509.CertificateStore
 import Data.X509.Validation
 import System.X509
 
+-- local
 import Network.TLS
 import Network.TLS.Extra
+
+certificationErrorsToIgnore :: [FailedReason]
+certificationErrorsToIgnore =
+  [ UnknownCA
+  ]
+
+type CertValidationFunction =
+     CertificateStore
+  -> ValidationCache
+  -> ServiceID
+  -> CertificateChain
+  -> IO [FailedReason]
+
+validateButIgnore :: CertValidationFunction
+validateButIgnore c v s ch = do
+  err <- validateDefault c v s ch
+  return $ err \\ certificationErrorsToIgnore
 
 initClientParams :: HostName -> IO ClientParams
 initClientParams hn = do
@@ -15,4 +38,5 @@ initClientParams hn = do
   return (defaultParamsClient hn "irc2me-server")
     { clientSupported = def { supportedCiphers = ciphersuite_strong }
     , clientShared    = def { sharedCAStore = cstore }
+    , clientHooks     = def { onServerCertificate = validateButIgnore }
     }
