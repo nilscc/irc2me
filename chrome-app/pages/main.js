c92fe447554a628b14caac248f599e7fcd83938b
Nils Schweinsberg <mail@nils.cc>
2014-12-22 16:26:06 +0100

    Simple backlog + sending & receiving

--- chrome-app/pages/main.js
+++ chrome-app/pages/main.js
@@ -1,24 +1,4 @@
 /*
- * Handlebars helper
- *
- */
-
-/*
-Handlebars.registerHelper('formatTimestamp', function (timestamp) {
-    return (new Date(timestamp)).toLocaleTimeString();
-});
-
-Handlebars.registerHelper('userflagClass', function(userflag) {
-    // the 3 cases (see messages.proto)
-    switch (userflag) {
-        case 0:     { return "userflag-operator"; }
-        case 1:     { return "userflag-voice"; }
-        default:    { return ""; }
-    }
-});
-*/
-
-/*
  * Main page class
  *
  */
@@ -39,28 +19,106 @@ MainPage.prototype.listen = function () {
     Irc2me.Signals.incomingMessage.addListener(function (msg) {
         // loop over networks
         for (var n = 0; n < msg.networks.length; n++) {
-            var network = msg.networks[n];
-            self.Chatview.append(network.messages);
+            var network    = msg.networks[n],
+                network_id = dcodeIO.Long.prototype.toNumber.call(network.id);
+
+            if (network.messages && network.messages.length > 0) {
+                self.Backlog.append(network_id, network.messages);
+                self.Chatview.append(network_id, network.messages);
+            }
+
+            for (var c = 0; network.channels && c < network.channels.length; c++) {
+                var chan = network.channels[c];
+                self.Backlog.append(network_id, chan.name, chan.messages);
+                self.Chatview.append(network_id, chan.name, chan.messages);
+            }
         }
     });
 }
 
 /*
- * Chat view
+ * Backlog
+ *
+ */
+
+MainPage.prototype.Backlog = {
+    _backlog: {},
+};
+
+MainPage.prototype.Backlog.append = function (network_id, channel_name, messages) {
+
+    var self = this;
+
+    // optional 'channel_name' argument
+    if (typeof channel_name == "object") {
+        messages = channel_name;
+        channel_name = null;
+    }
+
+    // init and alias network backlog
+    var backlog = self._backlog[network_id] = self._backlog[network_id] || {};
+
+    for (var i = 0; i < messages.length; i++) {
+
+        var msg  = messages[i],
+            name = channel_name || msg.sever || (msg.user && msg.user.nick);
+
+        if (name) {
+            // init 'name' backlog
+            backlog[name] = backlog[name] || [];
+            // append message
+            backlog[name].push(msg);
+        } else {
+            console.log(msg);
+            console.error("Invalid Backlog.append message");
+        }
+    }
+}
+
+MainPage.prototype.Backlog.get = function (network_id, name) {
+
+    var self = this;
+
+    var backlog = self._backlog[network_id] || {};
+
+    return backlog[name] || [];
+}
+
+
+/*
+ * UI: Chat view
  *
  */
 
 MainPage.prototype.Chatview = {
+
 };
 
-MainPage.prototype.Chatview.load = function (channel /* optional */) {
+MainPage.prototype.Chatview.load = function (network_id, channel_name) {
     var self = this;
+
+    self.currentNetwork = network_id;
+    self.currentChannel = channel_name;
+
+    // clear current messages
+    $("#message-list").empty();
+
+    self.append(page.Backlog.get(network_id, channel_name));
 };
 
-MainPage.prototype.Chatview.append = function (messages) {
+MainPage.prototype.Chatview.append = function (network_id, channel_name, messages) {
     var self = this;
 
-    //console.log(Must
+    // optional network_id/channel_name -> use current values otherwise
+    if (typeof network_id == "object" && !(channel_name || messages)) {
+        messages = network_id;
+        channel_name = self.currentChannel;
+        network_id = self.currentNetwork;
+    }
+    // ignore invalid network/channel
+    else if (network_id != self.currentNetwork || channel_name != self.currentChannel) {
+        return;
+    }
 
     var src = $("#message-template").html();
 
@@ -70,17 +128,54 @@ MainPage.prototype.Chatview.append = function (messages) {
         var epoch = dcodeIO.Long.prototype.toNumber.call(msg.timestamp);
         var date  = new Date(epoch);
 
-        var compiled_template = Mustache.to_html(src, {
+        var template_data = {
             timestamp: date.toLocaleTimeString(),
-            userflag: "",
-            nickname: msg.user.nick,
-            username: msg.user.name,
-            host: msg.user.host,
             content: msg.content,
-        });
+        };
+
+        if (msg.from == "user") {
+            template_data.user = msg.user;
+            template_data.user.flag = "";
+        } else {
+            template_data.server = msg.server;
+        }
+
+        var compiled_template = Mustache.to_html(src, template_data);
 
         $("#message-list").append(compiled_template);
     }
+};
+
+MainPage.prototype.Chatview.send = function (text, cb) {
+
+    var self = this;
+
+    if (! (self.currentNetwork && self.currentChannel)) {
+        return;
+    }
+
+    // aliases
+    Irc2me.sendPrivateMessage({
+        network_id: self.currentNetwork,
+        to:         self.currentChannel,
+        text:       text,
+    }, cb);
+}
+
+MainPage.prototype.Chatview.bindKeyEvents = function () {
+
+    var self = this;
+
+    $("#input-prompt input").keypress(function (e) {
+
+        var input = $(this);
+
+        if (e.which == 13) { // enter key
+            self.send(input.val(), function () {
+                input.val("");
+            });
+        }
+    });
 }
 
 /*
@@ -93,6 +188,8 @@ $(document).ready(function () {
     page = new MainPage();
 
     page.Chatview.load();
+
+    page.Chatview.bindKeyEvents();
 
     page.listen();
 });
