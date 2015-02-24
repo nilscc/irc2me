46e00e30430e0a69c9cb2ab957fe22465b78b555
Nils Schweinsberg <mail@nils.cc>
2015-02-24 01:48:13 +0100

    Make require paths absolute

--- chrome-app/require-common.js
+++ chrome-app/require-common.js
@@ -3,29 +3,29 @@ require.config({
     paths: {
 
         // paths
-        libs:                           "../libs",
-        src:                            "../src",
+        libs:                           "/libs",
+        src:                            "/src",
 
         // require.js
-        require:                        "../libs/require",
-        text:                           "../libs/require-text",
+        require:                        "/libs/require",
+        text:                           "/libs/require-text",
 
         // proto buf libs
-        ProtoBuf:                       "../libs/ProtoBuf.min",
-        ByteBuffer:                     "../libs/ByteBufferAB.min",
-        Long:                           "../libs/Long.min",
+        ProtoBuf:                       "/libs/ProtoBuf.min",
+        ByteBuffer:                     "/libs/ByteBufferAB.min",
+        Long:                           "/libs/Long.min",
 
         // mustache templates
-        Mustache:                       "../libs/mustache.min",
+        Mustache:                       "/libs/mustache.min",
 
-        linkify:                        "../libs/linkify.amd.min",
-        "linkify-string":               "../libs/linkify-string.amd.min",
+        linkify:                        "/libs/linkify.amd.min",
+        "linkify-string":               "/libs/linkify-string.amd.min",
 
         // jquery + plugins
-        jquery:                         "../libs/jquery-2.1.1.min",
-        "jquery-ajax-blog-arraybuffer": "../libs/jquery-ajax-blog-arraybuffers",
-        "jquery-regex-filter":          "../libs/jquery-regex-filter",
-        "jquery-tiny-pubsub":           "../libs/jquery-tiny-pubsub.min",
+        jquery:                         "/libs/jquery-2.1.1.min",
+        "jquery-ajax-blog-arraybuffer": "/libs/jquery-ajax-blog-arraybuffers",
+        "jquery-regex-filter":          "/libs/jquery-regex-filter",
+        "jquery-tiny-pubsub":           "/libs/jquery-tiny-pubsub.min",
     },
 
     shim: {
