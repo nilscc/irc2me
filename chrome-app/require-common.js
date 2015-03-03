e13215ce3eaba8652c59cf6e91d8b43c51a1faee
Nils Schweinsberg <mail@nils.cc>
2015-03-03 22:04:13 +0100

    Update require config

--- chrome-app/require-common.js
+++ chrome-app/require-common.js
@@ -1,19 +1,33 @@
 require.config({
 
+    basePath: "/build",
+
+    map: {
+        "*": {
+            "src/libs/dcodeIO/ProtoBuf":    "ProtoBuf",
+            "src/libs/dcodeIO/ByteBuffer":  "ByteBuffer",
+            "src/libs/dcodeIO/Long":        "Long",
+        },
+    },
+
     paths: {
 
         // paths
         libs:                           "/libs",
-        src:                            "/src",
+        src:                            "/build",
+        build:                          "/build",
+        pages:                          "/build/pages",
+        proto:                          "/build/proto",
 
         // require.js
         require:                        "/libs/require",
         text:                           "/libs/require-text",
 
         // proto buf libs
-        ProtoBuf:                       "/libs/ProtoBuf.min",
-        ByteBuffer:                     "/libs/ByteBufferAB.min",
-        Long:                           "/libs/Long.min",
+        dcodeIO:                        "/libs/dcodeIO",
+        ProtoBuf:                       "/libs/dcodeIO/ProtoBuf.min",
+        ByteBuffer:                     "/libs/dcodeIO/ByteBufferAB.min",
+        Long:                           "/libs/dcodeIO/Long.min",
 
         // mustache templates
         Mustache:                       "/libs/mustache.min",
